b6651c39b8ed83ea8d921858e98f1d5e84a83d98
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2026-06-07 14:57:04 -0300

    Generate distribution

---
+++ distribution/index.js
@@ -0,0 +1,88 @@
+import { get_parser } from "./parser.js";
+
+const transformer = {
+	weak_negation([x]) {
+		return 1 - x
+	},
+
+	strong_negation([x]) {
+		const distinguished = transformer.distinction([x]);
+		return transformer.weak_negation([distinguished]);
+	},
+
+	distinction([x]) {
+		return x == 0 ? 0 : 1;
+	},
+
+	crisp_truthness([x]) {
+		const negated = transformer.weak_negation([x]);
+		const distinguished = transformer.distinction([negated]);
+		return transformer.weak_negation([distinguished]);
+	},
+
+	value([token]) {
+		return Number(token.value);
+	},
+
+	disjunction([x, y]) {
+		return Math.max(x, y);
+	},
+
+	conjunction([x, y]) {
+		const negated_x = transformer.weak_negation([x]);
+		const negated_y = transformer.weak_negation([y]);
+		const disjoined = transformer.disjunction([negated_x, negated_y]);
+		return transformer.weak_negation([disjoined]);
+	},
+
+	l_implication([x, y]) {
+		return Math.min(1, 1 - x + y);
+	},
+
+	j_implication([x, y]) {
+		const distinguished = transformer.distinction([x]);
+		const negated = transformer.weak_negation([distinguished]);
+		return transformer.disjunction([negated, y]);
+	},
+
+	l_bi_implication([x, y]) {
+		const implicated_x_y = transformer.l_implication([x, y]);
+		const implicated_y_x = transformer.l_implication([y, x]);
+		return transformer.conjunction([implicated_x_y, implicated_y_x]);
+	},
+};
+
+const parser = get_parser({ transformer });
+
+const inputField = document.querySelector("#input-field");
+const inputButton = document.querySelector("#input-button");
+const outputDiv = document.querySelector("#output");
+
+inputField.addEventListener("keydown", (event) => {
+	if (event.key === "Enter") {
+		calculate();
+	}
+})
+
+inputButton.addEventListener("click", () => {
+	calculate();
+})
+
+function calculate() {
+	const input = getInput();
+	console.log(input);
+	const result = parser.parse(input);
+	setOutput(result);
+}
+
+function getInput() {
+	return inputField.getValue();
+}
+
+function setOutput(latex) {
+	const mathDiv = document.createElement("math-div");
+	mathDiv.innerHTML = latex;
+
+	outputDiv.replaceChildren();
+	outputDiv.appendChild(mathDiv);
+}
