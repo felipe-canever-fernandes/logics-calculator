3f62f45ac48e72d43c86e00a08b8021b62394547
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2026-06-07 15:46:09 -0300

    Move transformer to own module

---
+++ distribution/logic/transformer.js
@@ -0,0 +1,42 @@
+export const transformer = {
+    weak_negation([x]) {
+        return 1 - x;
+    },
+    strong_negation([x]) {
+        const distinguished = transformer.distinction([x]);
+        return transformer.weak_negation([distinguished]);
+    },
+    distinction([x]) {
+        return x == 0 ? 0 : 1;
+    },
+    crisp_truthness([x]) {
+        const negated = transformer.weak_negation([x]);
+        const distinguished = transformer.distinction([negated]);
+        return transformer.weak_negation([distinguished]);
+    },
+    value([token]) {
+        return Number(token.value);
+    },
+    disjunction([x, y]) {
+        return Math.max(x, y);
+    },
+    conjunction([x, y]) {
+        const negated_x = transformer.weak_negation([x]);
+        const negated_y = transformer.weak_negation([y]);
+        const disjoined = transformer.disjunction([negated_x, negated_y]);
+        return transformer.weak_negation([disjoined]);
+    },
+    l_implication([x, y]) {
+        return Math.min(1, 1 - x + y);
+    },
+    j_implication([x, y]) {
+        const distinguished = transformer.distinction([x]);
+        const negated = transformer.weak_negation([distinguished]);
+        return transformer.disjunction([negated, y]);
+    },
+    l_bi_implication([x, y]) {
+        const implicated_x_y = transformer.l_implication([x, y]);
+        const implicated_y_x = transformer.l_implication([y, x]);
+        return transformer.conjunction([implicated_x_y, implicated_y_x]);
+    },
+};
--- source/index.ts
+++ source/index.ts
@@ -1,56 +1,5 @@
-import { get_parser, Token } from "./parser.js";
-
-const transformer = {
-	weak_negation([x]: [number]) {
-		return 1 - x
-	},
-
-	strong_negation([x]: [number]) {
-		const distinguished = transformer.distinction([x]);
-		return transformer.weak_negation([distinguished]);
-	},
-
-	distinction([x]: [number]) {
-		return x == 0 ? 0 : 1;
-	},
-
-	crisp_truthness([x]: [number]) {
-		const negated = transformer.weak_negation([x]);
-		const distinguished = transformer.distinction([negated]);
-		return transformer.weak_negation([distinguished]);
-	},
-
-	value([token]: [Token]) {
-		return Number(token.value);
-	},
-
-	disjunction([x, y]: [number, number]) {
-		return Math.max(x, y);
-	},
-
-	conjunction([x, y]: [number, number]) {
-		const negated_x = transformer.weak_negation([x]);
-		const negated_y = transformer.weak_negation([y]);
-		const disjoined = transformer.disjunction([negated_x, negated_y]);
-		return transformer.weak_negation([disjoined]);
-	},
-
-	l_implication([x, y]: [number, number]) {
-		return Math.min(1, 1 - x + y);
-	},
-
-	j_implication([x, y]: [number, number]) {
-		const distinguished = transformer.distinction([x]);
-		const negated = transformer.weak_negation([distinguished]);
-		return transformer.disjunction([negated, y]);
-	},
-
-	l_bi_implication([x, y]: [number, number]) {
-		const implicated_x_y = transformer.l_implication([x, y]);
-		const implicated_y_x = transformer.l_implication([y, x]);
-		return transformer.conjunction([implicated_x_y, implicated_y_x]);
-	},
-};
+import { get_parser } from "./parser.js";
+import { transformer } from "./logic/transformer.js";
 
 const parser = get_parser({ transformer });
 
