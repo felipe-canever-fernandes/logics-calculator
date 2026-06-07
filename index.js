2b686a6323e091364bc68e4aafa48bfdc2a836e4
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2026-06-07 00:06:48 -0300

    Implement negation

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
+import { get_parser } from "./parser.js";
+
+const transformer = {
+	negation([x]) {
+		return 1 - x
+	},
+
+	value([token]) {
+		return Number(token.value);
+	}
+};
+
+const parser = get_parser({transformer});
+
 const inputField = document.querySelector("#input-field");
 const inputButton = document.querySelector("#input-button");
 const outputDiv = document.querySelector("#output");
 
 inputButton.addEventListener("click", () => {
-	const latex = inputField.getValue();
-	setOutput(latex);
+	const input = getInput();
+	const result = parser.parse(input);
+	setOutput(result);
 })
+
+function getInput() {
+	return inputField.getValue();
+}
 
 function setOutput(latex) {
 	const mathDiv = document.createElement("math-div");
