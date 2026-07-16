20150e68cd5bb0a9824a0646e359b980e99f4a2a
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2026-07-16 13:19:26 -0300

    Stop tracking distribution directory

--- distribution/ui/dom.js
+++
@@ -1,4 +0,0 @@
-export const inputField = document
-    .querySelector("#input-field");
-export const outputList = document
-    .querySelector("#output");
--- distribution/ui/invalid-result.js
+++
@@ -1,6 +0,0 @@
-export class InvalidResult extends Error {
-    constructor(message) {
-        super(message);
-        this.name = "InvalidResult";
-    }
-}
